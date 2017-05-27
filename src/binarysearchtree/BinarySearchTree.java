ddccc6b744ca48b21d8af11368c3d306b68ae088
Guillermo-C <guillermoicolin@hotmail.com>
2017-05-26 19:07:14 -0700

    Created a menu to show and execute all functions: add, remove and height functions.

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -5,6 +5,8 @@
  */
 package binarysearchtree;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ninjamemo
@@ -15,12 +17,53 @@ public class BinarySearchTree {
      * @param args the command line arguments
      */
     
+    static Scanner input = new Scanner(System.in);
     
+    static Tree tree = new Tree();
+    
+    public static void menu(){
+        
+        int choice = 0;
+        while (choice != -1){
+            
+            System.out.println("\n0: Add node");
+            System.out.println("1: Remove node");
+            System.out.println("2: Height of the tree");
+            System.out.println("3: Print tree (preorder)");
+            
+            
+            System.out.println("\nEnter an integer from the available choices (enter -1 to exit):\n");
+            choice = input.nextInt();   input.nextLine();
+            doMenu(tree,choice);
+        }
+    }
+    
+    public static void doMenu(Tree tree, int choice){
+        int userInput = 0;
+        
+        switch(choice){
+            case 0: System.out.println("\nType a non-existing value: ");
+                    userInput = input.nextInt();    input.nextLine();
+                    tree.add(userInput);
+                    break;
+            case 1: System.out.println("\nType an existing value: ");
+                    userInput = input.nextInt();    input.nextLine();
+                    tree.remove(userInput);
+                    break;
+            case 2: System.out.println("\nThe height of the tree is: " + tree.getHeight(tree.root));                
+                    break;                   
+            case 3: tree.printTree(tree.root);
+                    break;
+            
+            default: 
+                    break;
+        }
+    }
     
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Tree myTree = new Tree();
+        //Tree myTree = new Tree();
         
         /*        myTree.add(44);
         myTree.add(94);
@@ -31,20 +74,24 @@ public class BinarySearchTree {
         myTree.add(14);
         myTree.add(4);
         myTree.add(-4);*/
-        myTree.add(50);
+        
+        //  For testing
+        /*        myTree.add(50);
         myTree.add(30);
         myTree.add(60);
         myTree.add(58);
         myTree.add(65);
         myTree.add(63);
-        myTree.add(68);
+        myTree.add(68);*/
         
         
-        myTree.printTree(myTree.root);
-        System.out.println();
-        System.out.println("Height of the tree: " + myTree.getHeight(myTree.root));
-        myTree.remove(60);
-        myTree.printTree(myTree.root);
+        //myTree.printTree(myTree.root);
+        //System.out.println();
+        //System.out.println("Height of the tree: " + myTree.getHeight(myTree.root));
+        //myTree.remove(60);
+        //myTree.printTree(myTree.root);
+        
+        menu();
         
     }
     
